ebe89bff3195089dc1c4e1118e4517ce11865357
Angad Singh <singh04angad@gmail.com>
2024-02-29 22:33:06 +0530

    refernce v and typecasting

---
+++ cpp_reference_typecasting.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    /*********reference variable*********/
+    
+    // int x;
+    // x = 100;
+    // int &y = x;
+    // cout<<"value of x is: "<<x<<endl;
+    // cout<<"value of y is: "<<y<<endl;
+
+    /*********typecasting*********/
+
+    // int a = 100;
+    // float b = 100.5;
+    // int c;
+    // cout<<"value of a is "<<float(a)<<endl;
+    // cout<<"value of a is "<<(float)a<<endl;
+    // cout<<"value of b is "<<int(b)<<endl;
+    // cout<<"value of b is "<<(int)b<<endl;
+    // c = (int)b;
+    // cout<<"value of c is "<<c<<endl;
+
+}
