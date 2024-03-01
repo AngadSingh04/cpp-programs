63ad8219aeaf7837c76e5fc185014e5dd62c7536
Angad Singh <singh04angad@gmail.com>
2024-03-01 23:40:07 +0530

    pointers

---
+++ cpp_pointers.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int a = 5;
+    // int *b = &a;
+    // int **c = &b;
+    // cout<<a<<endl<<&a<<endl<<b<<endl<<*b<<endl<<c<<endl<<*c<<endl<<**c<<endl;
+
+    // int marks[4];
+    // marks[0]=10;
+    // marks[1]=20;
+    // marks[2]=30;
+    // marks[3]=40;
+
+    // int *a = marks;
+    // cout<<a<<endl<<*a<<endl<<*(a+1)<<endl<<*(a+2)<<endl<<*(a+3)<<endl;
+
+    // int *a = marks;
+    // cout<<*(a++)<<endl;
+    // cout<<*a<<endl;
+
+    // int *a = marks;
+    // cout<<*(++a)<<endl;
+    // cout<<*a<<endl;
+
+}
+
