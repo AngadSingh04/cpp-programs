d17a6f65a68332e60a536ae8cbec5a0e55dfef9f
Angad Singh <singh04angad@gmail.com>
2024-03-02 21:35:37 +0530

    struct,union and enum

---
+++ cpp_struct_union_enum.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+/**** Struct ***/
+
+// struct employee
+    // {
+    //     int em_id;
+    //     char fav;
+    //     int salary;
+    // } ;
+    
+    // int main(){
+    //     struct employee angad;
+    //     struct employee singh;
+    //     angad.em_id = 1;
+    //     singh.em_id = 2;
+    //     angad.fav = 'a';
+    //     angad.salary = 100;
+    //     cout<<angad.em_id<<endl<<angad.fav<<endl<<angad.salary<<endl<<singh.em_id<<endl;
+
+
+    // }
+
+/**** Union *****/
+
+
+    // union employee
+    // {
+    //     int a;
+    //     float b;
+    //     char c;
+    // };
+    
+    // int main(){
+    //     union employee angad;
+    //     angad.a = 20;
+    //     angad.b = 10.5f;
+    //     // angad.c = 'c';
+    //     cout<<angad.a<<endl<<angad.b<<endl;
+    // }
+
+/**** eNum ****/
+
+    // int main(){
+    //     enum days {monday,tuesday,wednesday,thursday,friday,saturday,sunday};
+    //     days d1 = wednesday;
+    //     cout<<d1<<endl;
+    //     cout<<saturday<<endl;
+    //     cout<<sunday<<endl;
+    // }
