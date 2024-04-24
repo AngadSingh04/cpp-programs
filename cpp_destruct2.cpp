dcc840cb249480929cdaf92ff47cebfe10d55bf6
Angad Singh <singh04angad@gmail.com>
2024-04-24 10:51:46 +0530

    templates basic

---
+++ cpp_destruct2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Base{
+    public:
+        Base(int a = 5){
+            cout<<"value if a is "<<a<<endl;
+        }
+        
+};
+int main(){
+
+
+    return 0;
+}
