8d90f376929c520775951f9bcff97d6ae693ff44
Angad Singh <singh04angad@gmail.com>
2024-05-04 13:54:27 +0530

    file handling 2

--- cpp_fileHandling.cpp
+++ cpp_fileHandling.cpp
@@ -4,8 +4,18 @@
 using namespace std;
 
 int main(){
-    string abc = "Hello My Name Is Angad Singh";
-    ofstream outputFile("cpp_SampleText.txt");
-    outputFile << abc;
+    // write
+    // string abc = "Hello My Name Is Angad Singh";
+    // ofstream outputFile("cpp_SampleText.txt");
+    // outputFile << abc;
+
+    // read
+    // string xyz;
+    // ifstream inputFile("cpp_SampleText2.txt");
+    // // inputFile >> xyz;
+    // getline(inputFile, xyz);
+    // cout << xyz;
+
+
     return 0;
 }
