6f4b718dba67fe4d0ad9016d07515539e0223ae9
Angad Singh <singh04angad@gmail.com>
2024-05-07 19:47:57 +0530

    updated

--- cpp_exception_handling2.cpp
+++ cpp_exception_handling2.cpp
@@ -4,10 +4,14 @@ using namespace std;
 int main(){
     string s = "four";
     try{
-        cout << s.at(4);
+        // cout << s.at(4);
+        int **myarr = new int*[99999999999](); 
     }
     catch(out_of_range& e){
         cout<<"Exception: "<<e.what()<<endl;  //Exception: basic_string::at: __n (which is 4) >= this->size() (which is 4)
+    }
+    catch(bad_alloc& e){
+        cout<<"Second catch: "<<e.what()<<endl; //Second catch: std::bad_alloc
     }
     
     return 0;
