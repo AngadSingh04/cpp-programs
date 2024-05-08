18eb5a0d5310155c1878be936407415445ecea91
Angad Singh <singh04angad@gmail.com>
2024-05-08 14:14:56 +0530

    updated

--- cpp_binary_to_decimal.cpp
+++ cpp_binary_to_decimal.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
 
+/*   first method   */
+
+
 int binary_to_decimal(int n){
     int num = n;
     int dec_value = 0;
@@ -19,3 +22,35 @@ int main(){
     cout << "Binary to Decimal is : "<<binary_to_decimal(n)<<endl;
     return 0;
 }
+
+
+/*   second method  */
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+
+// int binaryToDecimal(string n)
+// {
+// 	string num = n;
+// 	int dec_value = 0;
+
+// 	int base = 1;
+
+// 	int len = num.length();
+// 	for (int i = len - 1; i >= 0; i--) {
+// 		if (num[i] == '1')
+// 			dec_value += base;
+// 		base = base * 2;
+// 	}
+
+// 	return dec_value;
+// }
+
+
+// int main()
+// {
+// 	string num = "10101001";
+// 	cout << binaryToDecimal(num) << endl;
+// }
