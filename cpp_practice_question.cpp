c4dba07e706a93581f638a26b0c9b9b71f43a13f
Angad Singh <singh04angad@gmail.com>
2024-06-06 19:13:26 +0530

    practice question

--- cpp_practice_question.cpp
+++ cpp_practice_question.cpp
@@ -1,48 +1,28 @@
 #include<iostream>
-#include<cmath>
 using namespace std;
 
-class SimpleCalculator{
-    protected:
-        float a,b;
-    public:
-        void setData(float x,float y){
-            a = x;
-            b = y;
-        }
-        void opearations(){
-            cout<<"Sum "<<(a+b)<<endl;
-            cout<<"Subtract "<< (a-b)<<endl;
-            cout<<"Multiply "<<(a*b)<<endl;
-            cout<<"Divide "<<(a/b)<<endl;
-        }
-        
-};  
-class ScientificCalculator{
-    protected:
-        float c,d;
-    public:
-        void setData2(float x1, float y1){
-            c = x1;
-            d = y1;
-        }
-        void c_opearations(){
-            cout<<"Power "<<pow(c,d)<<endl;
-            cout<<"Square root "<<sqrt(c)<<endl;
-            cout<<"Sine"<<"("<<c<<") is "<<sin((c*M_PI)/180)<<endl;
-        }
-};
-class HybridCalculator: public SimpleCalculator, public ScientificCalculator{
-    public:
-        void display_all(){
-            opearations();
-            c_opearations();
-        }
-};
+
+
 int main(){
-    HybridCalculator hc;
-    hc.setData(5,10);
-    hc.setData2(45,3);
-    hc.display_all();
+    int rows = 3;
+    int cols = 4;
+    int **p = new int*[rows];
+    for(int i = 0; i<rows;i++){
+        p[i] = new int[cols];
+    }
+    for(int i = 0; i<rows;i++){
+        for(int j = 0; j<cols; j++){
+            p[i][j] = i+j;
+        }
+    }
+    for(int i =0;i<rows;i++){
+        for(int j =0;j<cols ;j++){
+            cout<< p[i][j]<<endl;
+        }
+    }
+    for(int i = 0; i<rows;i++){
+        delete[] p[i];
+    }
+    delete[] p;
     return 0;
 }
